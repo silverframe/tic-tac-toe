51bd29cb84a20d796506cf49c36dd9ee3d0c15b9
silverframe <stefanieseah@Stefanies-MacBook-Pro.local>
2016-03-01 16:06:17 +0800

    changed the js

--- tictactoe2.js
+++ tictactoe2.js
@@ -9,28 +9,28 @@ var counter = 0;
 
 var squares = $('.square');
 
-var winnerOrNot = false; 
+var winnerOrNot = false;
 
-squares.on('click', awesome);  
+squares.on('click', awesome);
 
 function winner () {
 
 var one = $("#S1").text();
-var two = $('#S2').text(); 
+var two = $('#S2').text();
 var three = $("#S3").text();
-var four = $('#S4').text(); 
+var four = $('#S4').text();
 var five = $("#S5").text();
-var six = $('#S6').text(); 
+var six = $('#S6').text();
 var seven = $("#S7").text();
-var eight = $('#S9').text(); 
+var eight = $('#S8').text(); 
 var nine = $("#S9").text();
-  if ( (one === two && two === three && three !=="" ) 
+  if ( (one === two && two === three && three !=="" )
     || (four === five && five === six && six !=="") || (seven === eight && eight === nine && nine !== "") ||
     (one === five && five === nine && nine !== "") || (three === five && five === seven && seven !=="") ||(one === four && four ===seven && seven !== "") ||
-    (two === five && five === eight && eight!== "") || (three === six && six === nine && nine !=="") ) 
+    (two === five && five === eight && eight!== "") || (three === six && six === nine && nine !=="") )
   { console.log("winner");
     alert("Player " + currentPlayer + " is the winner!" );
-    reset(); 
+    reset();
     window.location.reload()
   }
 }
@@ -53,16 +53,16 @@ function awesome () {
     if (counter % 2  === 0 ) {
       // $(this).css('backgroundColor', '#81F7F3');
       $(this).addClass('X').text("X");
-      $(this).removeClass('square-hover'); 
-      currentPlayer = "X";    
+      $(this).removeClass('square-hover');
+      currentPlayer = "X";
     } else if (counter % 2 === 1 ) {
       // $(this).css('backgroundColor', '#F5A9F2');
       $(this).addClass('O').text("O");
       $(this).removeClass('square-hover');
-      currentPlayer = "O"; 
+      currentPlayer = "O";
     }
-    
-    winner (); 
+
+    winner ();
     counter ++ ;
     checkTie ();
     console.log(counter);
